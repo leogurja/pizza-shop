893dd33b896b9561ce1ccaa07fd8ad935a08575f
Leonardo Gurgel <leonardogurgelmf@gmail.com>
2024-08-15 18:48:17 -0300

    initial config

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import { AppLayout } from "./pages/_layouts/App";
+import { AuthLayout } from "./pages/_layouts/Auth";
+import { Dashboard } from "./pages/app/Dashboard";
+import { SignIn } from "./pages/auth/SignIn";
+
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <AppLayout />,
+		children: [{ path: "/", element: <Dashboard /> }],
+	},
+	{
+		path: "/",
+		element: <AuthLayout />,
+		children: [{ path: "/sign-in", element: <SignIn /> }],
+	},
+]);
