b9651380b5184464ba206fee56497e29587bb1b0
Leonardo Gurgel <leonardogurgelmf@gmail.com>
2024-09-11 18:27:20 -0300

    add theme toggle

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,17 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 import { router } from "./routes";
 
 export function App() {
 	return (
-		<HelmetProvider>
-			<Helmet titleTemplate="%s | pizza.shop" />
-			<Toaster richColors closeButton />
-			<RouterProvider router={router} />
-		</HelmetProvider>
+		<ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+			<HelmetProvider>
+				<Helmet titleTemplate="%s | pizza.shop" />
+				<Toaster richColors closeButton />
+				<RouterProvider router={router} />
+			</HelmetProvider>
+		</ThemeProvider>
 	);
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Separator } from "@/components/ui/separator";
 import { Home, Pizza, UtensilsCrossed } from "lucide-react";
+import { AccountMenu } from "./account-menu";
 import { NavLink } from "./nav-link";
+import { ThemeToggle } from "./theme/theme-toggle";
 
 export function Header() {
 	return (
@@ -20,6 +22,10 @@ export function Header() {
 						Pedidos
 					</NavLink>
 				</nav>
+				<div className="ml-auto flex items-center gap-2">
+					<ThemeToggle />
+					<AccountMenu />
+				</div>
 			</div>
 		</div>
 	);
