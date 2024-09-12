ff491f66613341abfef95c985f635c39a5343df4
Leonardo Gurgel <leonardogurgelmf@gmail.com>
2024-09-12 16:37:35 -0300

    add pagination

--- src/pages/orders/orders.tsx
+++ src/pages/orders/orders.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "@/components/pagination";
 import {
 	Table,
 	TableBody,
@@ -15,32 +16,35 @@ export function Orders() {
 			<Helmet title="Pedidos" />
 			<div className="flex flex-col gap-4">
 				<h1 className="font-bold text-3xl tracking-tight">Pedidos</h1>
-			</div>
-			<div className="space-y-2.5">
-				<OrderTableFilters />
 
-				<div className="rounded-md border">
-					<Table>
-						<TableHeader>
-							<TableRow>
-								<TableHead className="w-[64px]"> </TableHead>
-								<TableHead className="w-[140px]">Identificador</TableHead>
-								<TableHead className="w-[180px]">Realizado há</TableHead>
-								<TableHead className="w-[140px]">Status</TableHead>
-								<TableHead>Cliente</TableHead>
-								<TableHead className="w-[140px]">Total do pedido</TableHead>
-								<TableHead className="w-[164px]"> </TableHead>
-								<TableHead className="w-[132px]"> </TableHead>
-							</TableRow>
-						</TableHeader>
+				<div className="space-y-2.5">
+					<OrderTableFilters />
 
-						<TableBody>
-							{Array.from({ length: 10 }).map((_, i) => {
-								// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-								return <OrderTableRow key={i} />;
-							})}
-						</TableBody>
-					</Table>
+					<div className="rounded-md border">
+						<Table>
+							<TableHeader>
+								<TableRow>
+									<TableHead className="w-[64px]"> </TableHead>
+									<TableHead className="w-[140px]">Identificador</TableHead>
+									<TableHead className="w-[180px]">Realizado há</TableHead>
+									<TableHead className="w-[140px]">Status</TableHead>
+									<TableHead>Cliente</TableHead>
+									<TableHead className="w-[140px]">Total do pedido</TableHead>
+									<TableHead className="w-[164px]"> </TableHead>
+									<TableHead className="w-[132px]"> </TableHead>
+								</TableRow>
+							</TableHeader>
+
+							<TableBody>
+								{Array.from({ length: 10 }).map((_, i) => {
+									// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+									return <OrderTableRow key={i} />;
+								})}
+							</TableBody>
+						</Table>
+					</div>
+
+					<Pagination pageIndex={0} totalCount={105} perPage={10} />
 				</div>
 			</div>
 		</>
