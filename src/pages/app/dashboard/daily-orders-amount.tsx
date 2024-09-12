feba064fd0b38161b6755b407b06a151e2c60460
Leonardo Gurgel <leonardogurgelmf@gmail.com>
2024-09-12 17:29:42 -0300

    add dashboard stats

---
+++ src/pages/app/dashboard/daily-orders-amount.tsx
@@ -0,0 +1,20 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Utensils } from "lucide-react";
+
+export function DailyOrdersAmount() {
+	return (
+		<Card>
+			<CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
+				<CardTitle className="font-semibold text-base">Pedidos (dia)</CardTitle>
+				<Utensils className="size-4 text-muted-foreground" />
+			</CardHeader>
+			<CardContent className="space-y-1">
+				<span className="font-bold text-2xl tracking-tight">12</span>
+				<p className="text-muted-foreground text-xs">
+					<span className="text-rose-500 dark:text-rose-400">-4%</span> em
+					relação ao mês passado.
+				</p>
+			</CardContent>
+		</Card>
+	);
+}
--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,10 +1,23 @@
 import { Helmet } from "react-helmet-async";
+import { DailyOrdersAmount } from "./daily-orders-amount";
+import { MonthlyCanceledOrdersAmount } from "./monthly-canceled-orders-amount";
+import { MonthlyOrdersAmount } from "./monthly-orders-amount";
+import { MonthlyRevenueCard } from "./monthly-revenue";
 
 export function Dashboard() {
 	return (
 		<>
 			<Helmet title="Dashboard" />
-			<h1>Dashboard</h1>
+			<div className="flex flex-col gap-4">
+				<h1 className="font-bold text-3xl tracking-tight">Dashboard</h1>
+
+				<div className="grid grid-cols-4 gap-4">
+					<MonthlyRevenueCard />
+					<MonthlyOrdersAmount />
+					<DailyOrdersAmount />
+					<MonthlyCanceledOrdersAmount />
+				</div>
+			</div>
 		</>
 	);
 }
