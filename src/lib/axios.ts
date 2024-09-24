0b40d5a625ec6d595ad6c9a0d990a67a1345f11a
Leonardo Gurgel <leo@gurgel.io>
2024-09-24 16:14:52 -0300

    auto redirect to login

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,22 @@
-import axios from "axios";
+import { router } from "@/routes";
+import axios, { type AxiosResponse } from "axios";
 import { env } from "../env";
 
 export const api = axios.create({
 	baseURL: env.VITE_API_URL,
 	withCredentials: true,
 });
+
+api.interceptors.response.use(
+	(r) => r,
+	(error: { response?: AxiosResponse }) => {
+		if (error.response == null) return error;
+		if (error.response.status === 401) {
+			router.navigate("/sign-in");
+		}
+		return error;
+	},
+);
 
 if (env.VITE_ENABLE_API_DELAY) {
 	api.interceptors.request.use(async (config) => {
