1fb4c21b1ea32ed0febae9a66d227df05207dd34
Leonardo Gurgel <leo@gurgel.io>
2024-09-24 16:42:46 -0300

    add order status change

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,9 +1,13 @@
-import type { Order } from "@/@types/order";
+import type { Order, OrderStatus as OrderStatusType } from "@/@types/order";
+import { approveOrder } from "@/api/approve-order";
 import { cancelOrder } from "@/api/cancel-order";
+import { deliverOrder } from "@/api/deliver-order";
+import { dispatchOrder } from "@/api/dispatch-order";
 import type { GetOrdersResponse } from "@/api/get-orders";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
+import { cn } from "@/lib/utils";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
@@ -16,31 +20,68 @@ interface OrderTableRowProps {
 	order: Order;
 }
 
+const NEXT_STATUS: Record<OrderStatusType, OrderStatusType> = {
+	pending: "processing",
+	processing: "delivering",
+	delivering: "delivered",
+	canceled: "canceled",
+	delivered: "delivered",
+};
+
+const ADVANCE_STATUS: Record<
+	OrderStatusType,
+	(order: { orderId: string }) => void
+> = {
+	pending: approveOrder,
+	processing: dispatchOrder,
+	delivering: deliverOrder,
+	canceled: () => {},
+	delivered: () => {},
+};
+
+const NEXT_STATUS_TEXT: Record<OrderStatusType, string> = {
+	pending: "Aprovar",
+	processing: "Entregar",
+	delivering: "Finalizar Entrega",
+	canceled: "",
+	delivered: "delivered",
+};
+
 export function OrderTableRow({ order }: OrderTableRowProps) {
 	const [isOpen, setIsOpen] = useState(false);
 	const queryClient = useQueryClient();
 
-	const { mutateAsync: handleCancelOrder } = useMutation({
-		mutationFn: cancelOrder,
-		onSuccess(_, { orderId }) {
-			const cachedQueries = queryClient.getQueriesData<GetOrdersResponse>({
-				queryKey: ["orders"],
+	const updateCachedOrder = (orderId: string, status: OrderStatusType) => {
+		const cachedQueries = queryClient.getQueriesData<GetOrdersResponse>({
+			queryKey: ["orders"],
+		});
+		for (const [cachedKey, cachedData] of cachedQueries) {
+			if (cachedData == null) continue;
+
+			queryClient.setQueryData<GetOrdersResponse>(cachedKey, {
+				...cachedData,
+				orders: cachedData.orders.map((order) => {
+					if (order.orderId !== orderId) return order;
+
+					return { ...order, status };
+				}),
 			});
-			for (const [cachedKey, cachedData] of cachedQueries) {
-				if (cachedData == null) continue;
+		}
+	};
 
-				queryClient.setQueryData<GetOrdersResponse>(cachedKey, {
-					...cachedData,
-					orders: cachedData.orders.map((order) => {
-						if (order.orderId !== orderId) return order;
+	const { mutateAsync: handleAdvanceOrder, isPending: isAdvancingOrder } =
+		useMutation({
+			mutationFn: async (params: { orderId: string }) =>
+				ADVANCE_STATUS[order.status](params),
+			onSuccess: (_, { orderId }) =>
+				updateCachedOrder(orderId, NEXT_STATUS[order.status]),
+		});
 
-						return { ...order, status: "canceled" };
-					}),
-				});
-			}
-			queryClient;
-		},
-	});
+	const { mutateAsync: handleCancelOrder, isPending: isCancelingOrder } =
+		useMutation({
+			mutationFn: cancelOrder,
+			onSuccess: (_, { orderId }) => updateCachedOrder(orderId, "canceled"),
+		});
 
 	return (
 		<TableRow>
@@ -75,17 +116,28 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
 				})}
 			</TableCell>
 			<TableCell className="">
-				<Button variant="ghost" size="xs">
-					<ArrowRight className="mr-2 size-3" /> Aprovar
+				<Button
+					variant="ghost"
+					size="xs"
+					onClick={() => handleAdvanceOrder(order)}
+					className={cn({
+						hidden: ["canceled", "delivered"].includes(order.status),
+					})}
+					disabled={isAdvancingOrder || isCancelingOrder}
+				>
+					<ArrowRight className="mr-2 size-3" />{" "}
+					{NEXT_STATUS_TEXT[order.status]}
 				</Button>
 			</TableCell>
 			<TableCell className="">
 				<Button
 					variant="ghost"
 					size="xs"
-					disabled={["canceled", "delivering", "delivered"].includes(
-						order.status,
-					)}
+					disabled={
+						isAdvancingOrder ||
+						isCancelingOrder ||
+						["canceled", "delivering", "delivered"].includes(order.status)
+					}
 					onClick={() => handleCancelOrder(order)}
 				>
 					<X className="mr-2 size-3" /> Cancelar
