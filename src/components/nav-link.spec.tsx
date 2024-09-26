015fdefaaacbf2599940a6aaf0a4c04fed497ba5
Leonardo Gurgel <leo@gurgel.io>
2024-09-26 18:46:01 -0300

    add nav-link tests

---
+++ src/components/nav-link.spec.tsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { NavLink } from "./nav-link";
+
+describe("NavLink", () => {
+	it("should highlight when it's the right page", () => {
+		const wrapper = render(<NavLink to="/about">About</NavLink>, {
+			wrapper: ({ children }) => (
+				<MemoryRouter initialEntries={["/about"]}>{children}</MemoryRouter>
+			),
+		});
+
+		wrapper.debug();
+		expect(wrapper.getByText("About").getAttribute("aria-current")).toEqual(
+			"page",
+		);
+	});
+
+	it("should not highlight when it's the right page", () => {
+		const wrapper = render(<NavLink to="/about">About</NavLink>, {
+			wrapper: ({ children }) => (
+				<MemoryRouter initialEntries={["/"]}>{children}</MemoryRouter>
+			),
+		});
+
+		expect(wrapper.getByText("About").getAttribute("aria-current")).toBeNull();
+	});
+});
