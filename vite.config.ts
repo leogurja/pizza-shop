2f357b22422d6542104f2bf8e21976762c8ecdf2
Leonardo Gurgel <leo@gurgel.io>
2024-09-26 18:04:44 -0300

    add test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
@@ -9,5 +11,10 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
+	},
+	test: {
+		globals: true,
+		setupFiles: ["./test/setup.ts"],
+		environment: "happy-dom",
 	},
 });
