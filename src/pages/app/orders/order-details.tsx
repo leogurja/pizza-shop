e5246cff1d0a5a42578a9297dd8c30655416f74d
Leonardo Gurgel <leo@gurgel.io>
2024-09-26 17:16:35 -0300

    add order details skeleton

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -17,6 +17,7 @@ import { DialogDescription } from "@radix-ui/react-dialog";
 import { useQuery } from "@tanstack/react-query";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { OrderDetailsSkeleton } from "./order-details-skeleton";
 import { OrderStatus } from "./order-status";
 
 interface OrderDetailsProps {
@@ -37,7 +38,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
 				<DialogTitle>Pedido: {orderId}</DialogTitle>
 				<DialogDescription>Detalhes do pedido</DialogDescription>
 			</DialogHeader>
-			{data && (
+			{data ? (
 				<div className="space-y-6">
 					<Table>
 						<TableBody>
@@ -128,6 +129,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
 						</TableFooter>
 					</Table>
 				</div>
+			) : (
+				<OrderDetailsSkeleton />
 			)}
 		</DialogContent>
 	);
